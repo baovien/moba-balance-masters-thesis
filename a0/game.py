4f7b99b1edf1131cd4dacc2520752038eb6fbde0
Bao Vien Ngo <Bao.Ngo@forsta.com>
2021-12-15 18:26:30 +0100

    start a0

---
+++ a0/game.py
@@ -0,0 +1,157 @@
+import numpy as np
+import random
+
+class Dota2Draft:
+    """
+    A very, very simple game of ConnectX in which we have:
+        rows: 1
+        columns: 10
+        winNumber: 2
+    """
+
+    def __init__(self):
+        self.columns = 10
+        self.done_draft = 10
+
+    def get_init_board(self):
+        b = np.zeros((self.columns,), dtype=np.int)
+        return b
+
+    def get_board_size(self):
+        return self.columns
+
+    def get_action_size(self):
+        return self.columns
+
+    def get_next_state(self, board, player, action):
+        b = np.copy(board)
+        b[action] = player
+
+        # Return the new game, but
+        # change the perspective of the game with negative
+        return (b, -player)
+
+    def has_legal_moves(self, board):
+        for index in range(self.columns):
+            if board[index] == 0:
+                return True
+        return False
+
+    def get_valid_moves(self, board):
+        # All moves are invalid by default
+        valid_moves = [0] * self.get_action_size()
+
+        for index in range(self.columns):
+            if board[index] == 0:
+                valid_moves[index] = 1
+
+        return valid_moves
+
+    def is_draft_done(self, board, player):
+        count = 0
+        for index in range(self.columns):
+            if board[index] == player:
+                count = count + 1
+            else:
+                count = 0
+
+            if count == self.done_draft:
+                return True
+
+        return False
+
+    def virtual_loss(self):
+        return random.random()
+
+    def get_reward_for_player(self, board, player):
+        # return None if not ended, 1 if player 1 wins, -1 if player 1 lost
+
+        if self.is_draft_done(board, player):
+            return self.virtual_loss()
+        if self.is_draft_done(board, -player):
+            return 1 - self.virtual_loss()
+        if self.has_legal_moves(board):
+            return None
+
+        return 0
+
+    def get_canonical_board(self, board, player):
+        return player * board
+
+
+
+
+class Connect2Game:
+    """
+    A very, very simple game of ConnectX in which we have:
+        rows: 1
+        columns: 4
+        winNumber: 2
+    """
+
+    def __init__(self):
+        self.columns = 4
+        self.win = 2
+
+    def get_init_board(self):
+        b = np.zeros((self.columns,), dtype=np.int)
+        return b
+
+    def get_board_size(self):
+        return self.columns
+
+    def get_action_size(self):
+        return self.columns
+
+    def get_next_state(self, board, player, action):
+        b = np.copy(board)
+        b[action] = player
+
+        # Return the new game, but
+        # change the perspective of the game with negative
+        return (b, -player)
+
+    def has_legal_moves(self, board):
+        for index in range(self.columns):
+            if board[index] == 0:
+                return True
+        return False
+
+    def get_valid_moves(self, board):
+        # All moves are invalid by default
+        valid_moves = [0] * self.get_action_size()
+
+        for index in range(self.columns):
+            if board[index] == 0:
+                valid_moves[index] = 1
+
+        return valid_moves
+
+    def is_win(self, board, player):
+        count = 0
+        for index in range(self.columns):
+            if board[index] == player:
+                count = count + 1
+            else:
+                count = 0
+
+            if count == self.win:
+                return True
+
+        return False
+
+    def get_reward_for_player(self, board, player):
+        # return None if not ended, 1 if player 1 wins, -1 if player 1 lost
+
+        if self.is_win(board, player):
+            return 1
+        if self.is_win(board, -player):
+            return -1
+        if self.has_legal_moves(board):
+            return None
+
+        return 0
+
+    def get_canonical_board(self, board, player):
+        return player * board
+
---
+++ a0/main.py
@@ -0,0 +1,26 @@
+import torch
+
+from game import Connect2Game, Dota2Draft
+from model import Connect2Model
+from trainer import Trainer
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+args = {
+    'batch_size': 64,
+    'numIters': 100, #500                                # Total number of training iterations
+    'num_simulations': 100,                         # Total number of MCTS simulations to run when deciding on a move to play
+    'numEps': 100,                                  # Number of full games (episodes) to run during each iteration
+    'numItersForTrainExamplesHistory': 20,
+    'epochs': 2,                                    # Number of epochs of training per iteration
+    'checkpoint_path': 'latest.pth'                 # location to save latest set of weights
+}
+
+game = Connect2Game()
+board_size = game.get_board_size()
+action_size = game.get_action_size()
+
+model = Connect2Model(board_size, action_size, device)
+
+trainer = Trainer(game, model, args)
+trainer.learn()
