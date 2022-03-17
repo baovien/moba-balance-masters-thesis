41b91ca186393922286ab53acff3276bb29e1317
Bao <baovienngo@gmail.com>
2022-03-17 08:06:30 +0100

    Delete read_gzip.py

--- scraper/read_gzip.py
+++
@@ -1,7 +0,0 @@
-import gzip
-import tqdm
-n_samples = 0
-with gzip.open("data/matches_5148330922-5148330922.gz", 'rt') as fp:
-    for line in tqdm.tqdm(fp): 
-        n_samples += 1 
-    print(n_samples)
